1ba3e423c1949ed7470f73df59c70bd3577ecc81
Prajwal <imprjde7@gmail.com>
2023-11-23 09:18:49 +0530

    Minor changes

--- controller/Auth.js
+++ controller/Auth.js
@@ -7,9 +7,9 @@ const ProfileImage = imgModel.ProfileImage;
 
 exports.signupUser = async (req, res) => {
   try {
-    const checkEmail = await User.findOne({ email: req.body.email });
+    const validateEmail = await User.findOne({ email: req.body.email });
 
-    if (checkEmail) {
+    if (validateEmail) {
       return res.status(400).json({ message: "Email already exists" });
     }
 
